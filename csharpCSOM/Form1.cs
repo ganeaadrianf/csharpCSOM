73fd693e7e77590252a3a7e673a95f373d9297b3
ganeaadrianf <ganea.adrianf@gmail.com>
2017-01-16 08:27:54 -0800

    initial version

---
+++ csharpCSOM/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharpCSOM
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var context = new ClientContext("http://apps.shpdev.com"))
+            {
+                var lists = context.Web.Lists;
+                context.Load(lists);
+                context.ExecuteQuery();
+                listBox1.Items.Clear();
+                foreach (var item in lists)
+                {
+                    listBox1.Items.Add(item.Title);
+                }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var context = new ClientContext("http://apps.shpdev.com"))
+            {
+                var query = from list in context.Web.Lists
+                            where list.ItemCount>3
+                            select list;
+                var lists=context.LoadQuery(query);
+                context.ExecuteQuery();
+                listBox1.Items.Clear();
+                foreach (var item in lists)
+                {
+                    listBox1.Items.Add(item.Title);
+                }
+
+            }
+
+        }
+    }
+}
